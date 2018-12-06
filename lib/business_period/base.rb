8255f3069895f63acc35bae796f5379b3a4f2f20
matas <matas.simonaitis@ksdigital.lt>
2018-12-06 14:05:01 +0200

    bugfix

--- lib/business_period/base.rb
+++ lib/business_period/base.rb
@@ -8,11 +8,10 @@ module BusinessPeriod
 
     def calculate_period(to_date)
       @calculate_period ||= begin
-        magic_number = 8 - config.work_days.size
-
-        finish = DateTime.now.next_day(to_date * magic_number).to_date
-
-        Time.now.to_date..finish
+        magic_number = 10 - config.work_days.size
+        days_to_add = days_to_seconds(to_date * magic_number)
+        finish = Time.now + days_to_add
+        Time.now.to_date..finish.to_date
       end
     end
 
@@ -24,6 +23,10 @@ module BusinessPeriod
 
     def holiday_config
       [File.dirname(__FILE__), "../../config/holidays/#{config.locale}.yml"]
+    end
+
+    def days_to_seconds(days)
+      days * 60 * 60 * 24
     end
   end
 end
