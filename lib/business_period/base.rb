59decdf6aaa1c3f6c4fb20529807f8a5acefe9bd
matas <matas.simonaitis@ksdigital.lt>
2018-12-06 11:59:32 +0200

    removed comments

--- lib/business_period/base.rb
+++ lib/business_period/base.rb
@@ -6,24 +6,20 @@ module BusinessPeriod
       @config ||= Config
     end
 
-    # Returns range of days
     def calculate_period(to_date)
       @calculate_period ||= begin
         finish = DateTime.now.next_day(to_date).to_date
 
-        # Builds an array from current time to imaginable end time
         Time.now.to_date..finish
       end
     end
 
-    # Sets holiday instance by given file location
     def holidays
       @holidays ||= YAML.load_file(File.join(holiday_config)).fetch('months')
     end
 
     private
 
-    # Gets path to config file by given locale
     def holiday_config
       [File.dirname(__FILE__), "../../config/holidays/#{config.locale}.yml"]
     end
